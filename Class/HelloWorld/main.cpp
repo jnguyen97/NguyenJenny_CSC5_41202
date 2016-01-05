1db7b14c23a35c034173bf578bae20322eb7e72b
jnguyen97 <jnguyen97@student.rcc.edu>
2016-01-05 11:02:14 -0800

    Signed-off-by: jnguyen97 <jnguyen97@student.rcc.edu>

---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,32 @@
+/* 
+     File:   main.cpp
+     Author: Jenny Nguyen
+     Created on January 4, 2016, 10:18 AM
+     Purpose: Check out IDE
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    
+    //Input data
+    
+    //Calculate or map inputs to outputs
+    
+    //Output the results
+    cout<<"Hello World"<<endl;
+    
+    //Exit stage right
+    return 0;
+}
+
