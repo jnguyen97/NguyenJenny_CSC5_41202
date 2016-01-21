3b51c34995280196fcf44886b806a42cca747801
jnguyen97 <jnguyen97@student.rcc.edu>
2016-01-21 10:13:17 -0800

    Signed-off-by: jnguyen97 <jnguyen97@student.rcc.edu>

--- Homework/Assignment_3/MenuWithProblemsDecisions/main.cpp
+++ Homework/Assignment_3/MenuWithProblemsDecisions/main.cpp
@@ -29,10 +29,10 @@ int main(int argc, char** argv) {
     do{
          //Input data
         cout<<endl<<"Assignment 3 Problem Set"<<endl;
-        cout<<"Type 1 to Display Savitch 8th Chap3 Prob10"<<endl;
-        cout<<"Type 2 to Display Savitch 8th Chap3 Prob11"<<endl;
-        cout<<"Type 3 to Display Savitch 7th Chap3 Prob 1"<<endl;
-        cout<<"Type 4 to Display Problem"<<endl;
+        cout<<"Type 1 to Display Savitch 8Ed Chap3 Prob 10"<<endl;
+        cout<<"Type 2 to Display Savitch 8Ed Chap3 Prob 11"<<endl;
+        cout<<"Type 3 to Display Savitch 7Ed Chap3 Prob 1"<<endl;
+        cout<<"Type 4 to Display Savitch 9Ed Chap3 Prog Proj 3"<<endl;
         cout<<"Type anything else to exit"<<endl<<endl;
         cin>>nSoltn;
         //Solutions to all the problems
@@ -123,7 +123,7 @@ int main(int argc, char** argv) {
 
                     //Input the player's turn
                     do{
-                        cout<<"What is your move P, R, S? (｡◕‿‿◕｡)"<<endl;
+                        cout<<"What is your move P, R, S? =D "<<endl;
                         cin>>player;
                         player=toupper(player);
                     }while(!(player=='P'||player=='R'||player=='S'));
@@ -139,19 +139,19 @@ int main(int argc, char** argv) {
 
                     //Determine the result
                     if(computr==player){
-                        cout<<"Tie! No one wins! ƪ(˘‿˘)ʃ "<<endl;
+                        cout<<"Tie! No one wins! O: "<<endl;
                     }else if(player=='P'&&computr=='R'){
-                        cout<<"You Won! Good job! (ﾉ ◕‿◕)ﾉ*:･ﾟ✧"<<endl;
+                        cout<<"You Won! Good job! =D"<<endl;
                     }else if(player=='R'&&computr=='S'){
-                        cout<<"You Won! Awesome! ~(˘▾˘~)"<<endl;
+                        cout<<"You Won! Awesome! ^_^"<<endl;
                     }else if(player=='S'&&computr=='P'){
-                        cout<<"You Won! Nice! (づ｡◕‿‿◕｡)づ"<<endl;
+                        cout<<"You Won! Nice! :)"<<endl;
                     }else{
-                        cout<<"You lost! ｡゜(; ^ ;)゜｡"<<endl;
+                        cout<<"You lost! =("<<endl;
                     }
 
                     //Keep playing?
-                    cout<<endl<<"Would you like to play around round?(｡◕ ‿ ◕｡)"<<endl;
+                    cout<<endl<<"Would you like to play around round? =)"<<endl;
                     cout<<"(Press Y to play or press any other key to quit.)"<<endl;
                     cin>>qwstion;
                 }while(toupper(qwstion)=='Y');
@@ -160,9 +160,74 @@ int main(int argc, char** argv) {
                 }
             case 4:{
                 //The problem to solve
-                cout<<endl<<"Solution to Problem"<<endl;
-                cout<<endl<<"The"<<endl<<endl;
-                
+                cout<<endl<<"Solution to Savitch 9thEd Chap 3 Prog Proj3"<<endl;
+                cout<<endl<<"The Roman Numeral Conversion Program"<<endl<<endl;
+
+                //set random number seed
+                srand(static_cast<unsigned int>(time(0)));     
+
+                //Declare and initialize variables 
+                unsigned short number=rand()%2001+1000;  //[1000,3000]
+                unsigned char  n1000s,n100s,n10s,n1s;    //Number of 10^x
+                cout<<"The number to convert = "<<number<<endl;
+
+                //Calculate the number of 1000's, 100's, 10's, 1's
+                n1000s=(number-number%1000)/1000; //Number of 1000's
+                number=(number-n1000s*1000);      //Subtract off 1000's
+                n100s=(number-number%100)/100;    //Number of 100's
+                number=(number-n100s*100);        //Subtract off 100's
+                n10s=(number-number%10)/10;       //Number of 10's
+                n1s=(number-n10s*10);             //Subtract off 10's to get 1's
+
+                //Output the number and its components
+                cout<<"The number of 1000's  = "<<static_cast<int>(n1000s)<<endl;
+                cout<<"The number of 100's   = "<<static_cast<int>(n100s)<<endl;
+                cout<<"The number of 10's    = "<<static_cast<int>(n10s)<<endl;
+                cout<<"The number of 1's     = "<<static_cast<int>(n1s)<<endl;
+
+                cout<<"The Roman Numeral Equivalent = ";
+
+                //Output the 1000's
+                switch(n1000s){
+                    case 3: cout<<"M";
+                    case 2: cout<<"M";
+                    case 1: cout<<"M";
+                }
+                //Output the 100's
+                switch(n100s){
+                    case 9: cout<<"CM";break;
+                    case 8: cout<<"DCCC";break;
+                    case 7: cout<<"DCC";break;
+                    case 6: cout<<"DC";break;
+                    case 5: cout<<"D";break;
+                    case 4: cout<<"CD";break;
+                    case 3: cout<<"C";
+                    case 2: cout<<"C";
+                    case 1: cout<<"C";
+                }//Output the 100's
+                switch(n10s){
+                    case 9: cout<<"XC";break;
+                    case 8: cout<<"LXXX";break;
+                    case 7: cout<<"LXX";break;
+                    case 6: cout<<"LX";break;
+                    case 5: cout<<"L";break;
+                    case 4: cout<<"XL";break;
+                    case 3: cout<<"X";
+                    case 2: cout<<"X";
+                    case 1: cout<<"X";
+                }
+                //Output the 1's
+                switch(n1s){
+                    case 9: cout<<"IX";break;
+                    case 8: cout<<"VIII";break;
+                    case 7: cout<<"VII";break;
+                    case 6: cout<<"VI";break;
+                    case 5: cout<<"V";break;
+                    case 4: cout<<"IV";break;
+                    case 3: cout<<"III";break;
+                    case 2: cout<<"II";break;
+                    case 1: cout<<"I";break;
+                }
                     break;
             }
                 default:{
